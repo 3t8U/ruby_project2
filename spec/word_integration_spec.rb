58b805d21c111a162ce4081c7cd02c1b2970e8ee
Student <example@example.com>
2019-12-20 13:47:26 -0800

    finsh all testing

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -2,3 +2,25 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+
+describe('create a word path', {:type => :feature}) do
+  it('creates a word and then goes to the word page') do
+    visit('/words')
+    click_on('Add a new word')
+    fill_in('word_name', :with => 'SEAHORSE')
+    click_on('Go!')
+    expect(page).to have_content('SEAHORSE')
+  end
+end
+
+describe('create a definition path', {:type => :feature}) do
+  it('creates a word with a definition and then goes to the word page') do
+    word = Word.new("Seacrest", nil, nil,)
+    word.save
+    visit("/words/#{word.id}")
+    fill_in('definition_name', :with => 'Foamy & Aquamarine')
+    click_on('Add definition')
+    expect(page).to have_content('Foamy & Aquamarine')
+  end
+end
